34d7e1cd06384421d8eaa693d8f59501b9c4b0ba
ChrisLaptop <chrisgonzaco@gmail.com>
2022-10-05 13:28:52 +0200

    Control de entrada y salida

--- solidtime.py
+++ solidtime.py
@@ -3,11 +3,13 @@ from multiprocessing import connection
 import sqlite3 as sql
 import time
 
+
 def create_database():
     connection = sql.connect("solidtime.db")
     connection.commit()
-    connection.close() 
-    
+    connection.close()
+
+
 def create_table():
     connection = sql.connect("solidtime.db")
     cursor = connection.cursor()
@@ -19,7 +21,7 @@ def create_table():
         )"""
     )
 
-    
+
 def registro_entrada(dni):
     connection = sql.connect("solidtime.db")
     cursor = connection.cursor()
@@ -27,14 +29,14 @@ def registro_entrada(dni):
     cursor.execute(instruction)
     datos = cursor.fetchall()
     if datos[0][2] == "entrada":
-        print("No se puede generar un registro de entrada ya que el último registro existente que tiene su usuario es un registro de entrada.")
+        print(
+            "No se puede generar un registro de entrada ya que el último registro existente que tiene su usuario es un registro de entrada.")
     else:
         instruction = f"INSERT INTO control VALUES ({dni}, {time.time()}, 'entrada')"
         cursor.execute(instruction)
         connection.commit()
     connection.close()
-    
-    
+
 
 def registro_salida(dni):
     connection = sql.connect("solidtime.db")
@@ -43,12 +45,23 @@ def registro_salida(dni):
     cursor.execute(instruction)
     datos = cursor.fetchall()
     if datos[0][2] == "salida":
-        print("No se puede generar un registro de salida ya que el último registro existente que tiene su usuario es un registro de salida.")
+        print(
+            "No se puede generar un registro de salida ya que el último registro existente que tiene su usuario es un registro de salida.")
     else:
         instruction = f"INSERT INTO control VALUES ({dni}, {time.time()}, 'salida')"
         cursor.execute(instruction)
         connection.commit()
     connection.close()
-    
+
+
+def obtener_registros(dni):
+    connection = sql.connect("solidtime.db")
+    cursor = connection.cursor()
+    instruction = f"SELECT * FROM control WHERE dni = {dni} ORDER BY epoch DESC"
+    cursor.execute(instruction)
+    datos = cursor.fetchall()
+    connection.close()
+    return datos
+
 # create_database()
 # create_table()
